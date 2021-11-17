7f23cc421e75b05c9d19365f70f58fb4dd6eeab7
Martin M <65924165+Aredarn@users.noreply.github.com>
2021-11-17 20:59:06 +0100

    Javascript AniME update

---
+++ scripts.js
@@ -0,0 +1,45 @@
+var buttonMain = document.querySelectorAll('.hoverTd');
+var adClick = document.querySelectorAll(".pc");
+
+
+function animateButton(el, scale, duration, elasticity) {
+  anime.remove(el);
+  anime({
+    targets: el,
+    scale: scale,
+    duration: duration,
+    elasticity: elasticity
+  });
+}
+
+function enterButton(el) {
+  animateButton(el, 1.2, 800, 400)
+  console.log("saniy")
+};
+
+function leaveButton(el) {
+  animateButton(el, 1.0, 600, 300)
+};
+
+for (var i = 0; i < buttonMain.length; i++) {
+    buttonMain[i].addEventListener('mouseenter', function(e) {
+    enterButton(e.target);
+  }, false);
+  
+  buttonMain[i].addEventListener('mouseleave', function(e) {
+    leaveButton(e.target)
+  }, false);  
+}
+
+
+for (var i = 0; i < adClick.length; i++) {
+  adClick[i].addEventListener('mouseenter', function(e) {
+  enterButton(e.target);
+}, false);
+
+adClick[i].addEventListener('mouseleave', function(e) {
+  leaveButton(e.target)
+}, false);  
+}
+
+  
